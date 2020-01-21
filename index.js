09b44ef1162af6eb22c88200744a66f67aa5ba2c
anoriqq <shota.yoshikawa@anoriqq.com>
2020-01-21 22:48:28 +0900

    LINEのwebhookに対応した

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express')
-const path = require('path')
+const crypto = require('crypto');
 const PORT = process.env.PORT || 5000
 
+const CHANNEL_SECRET = '19290ca149214fb45d4964e4ca42ab8d';
+
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => res.send({ok: true}))
+  .post('/webhook', (req, res) => {
+    console.log(req.body);
+
+    const signature = crypto
+      .createHmac('SHA256', CHANNEL_SECRET)
+      .update(req.body)
+      .digest('base64');
+
+    console.log(signature);
+    // req の中の message を取り出す
+    // ニュースを取得する
+    // ニュースを返す
+    res.status(200).end();
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
