cde5f0dba9f02c4b8169b1229178ba3b685afc45
anoriqq <shota.yoshikawa@anoriqq.com>
2020-01-22 02:33:58 +0900

    flexのテスト

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const crypto = require('crypto');
 const bodyParser = require('body-parser');
+const got = require('got');
 const line = require('@line/bot-sdk');
+const parseXML = require('xml2js').parseStringPromise;
+
+const wrap = fn => (req, res, next) => fn(req, res, next).catch(next);
 
 const PORT = process.env.PORT || 5000;
 const CHANNEL_ACCESS_TOKEN = 'cl943kzifDNwXPWVjvSVLVGJXWW6eRnPr4Eb5rLEhbHriucDqJ5IZzpGR7LlRfrkTpIKhXBYm27SBj/lv3eNVgG+83Ds1kjRnPtwedecLtj/weaTpo6npRekFk/KC8gwxNqMedBz7jW33wQxRzt4mgdB04t89/1O/w1cDnyilFU=';
@@ -15,6 +19,13 @@ const client = new line.Client({
 express()
   .use(bodyParser.json())
   .use(bodyParser.urlencoded({ extended: false }))
+  .get('/test', wrap(async (req, res) => {
+    const text = req.query.q;
+    console.log('text: ', text);
+    if(!text) return res.end();
+    const data = await getNews(text);
+    return res.json({ data });
+  }))
   .post('/webhook', (req, res) => {
     const signature = crypto
       .createHmac('SHA256', CHANNEL_SECRET)
@@ -30,21 +41,52 @@ express()
     if(!events) return res.status(200).end();
 
     Promise.all(events.map(handleEvent))
-      .then(result=>res.json(result));
+      .then(result => res.json(result));
   })
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+  .listen(PORT, () => console.log(`Listening on http://localhost:${PORT}/test?q=%E3%83%97%E3%83%AA%E3%83%B3`));
 
 async function handleEvent(event) {
   if(event.type !== 'message' || event.message.type !== 'text') return;
 
   const text = event.message.text;
 
-  client.replyMessage(event.replyToken, {
-    type: 'text',
-    text: `https://www.google.com/search?q=${encodeURI(text)}`,
-  });
+  try {
 
-  // TODO ニュースを取得する
-  // TODO ニュースを返す
-  // https://www.google.com/search?q=%E3%83%97%E3%83%AA%E3%83%B3
+    const { title, news } = await getNews(text);
+
+    const tmpData = {
+      type: 'bubble',
+      body: {
+        type: 'box',
+        layout: 'horizontal',
+        contents: [
+          {
+            type: 'text',
+            text: 'Hello,',
+          },
+          {
+            type: 'text',
+            text: 'World!',
+          }
+        ]
+      }
+    }
+
+    client.replyMessage(event.replyToken, tmpData);
+  } catch(error) {
+    return console.error(error);
+  }
+}
+
+async function getNews(text) {
+  const response = await got(`https://news.google.com/rss/search?hl=ja&gl=JP&ceid=JP:ja&q=${encodeURI(text)}`);
+
+  const data = await parseXML(response.body)
+  const title = data.rss.channel[0].title[0];
+  const news = data.rss.channel[0].item.splice(3);
+
+  return {
+    title,
+    news,
+  };
 }
